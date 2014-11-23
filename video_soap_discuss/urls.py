d0b82c23cc45c145b1aa5cd2e7b1e7e840f16f1f
Wayne Koorts <wayne@wkoorts.com>
2014-11-23 22:21:36 +1300

    Add subject URLs.

--- video_soap_discuss/urls.py
+++ video_soap_discuss/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns("",
-    url(r"^$", "discuss.views.home", name = "home"),
+    url(r"^$", "discuss.views.home.index", name = "index"),
+
+    # Subjects.
+    url(r"^subjects/$", "discuss.views.subjects.index", name = "subjectIndex"),
+    url(r"^subjects/view/(\d+)/$", "discuss.views.subjects.view", name = "viewSubject"),
+
+    # Admin.
     url(r"^admin/", include(admin.site.urls)),
 )
