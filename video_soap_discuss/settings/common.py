4ddcf707752f6ba5d3f3549927fbc7fbd833e5d1
Wayne Koorts <wayne@wkoorts.com>
2014-11-23 19:20:19 +1300

    Divide settings up into common / dev etc.

--- video_soap_discuss/settings/common.py
+++ video_soap_discuss/settings/common.py
@@ -18,16 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "s=z)0o#cw02t2o+_=4oyj7w1e)&td2uci!z*xtj=ec+e87#(&2"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
-
 INSTALLED_APPS = (
     "django.contrib.admin",
     "django.contrib.auth",
@@ -49,39 +40,13 @@ MIDDLEWARE_CLASSES = (
 )
 
 ROOT_URLCONF = "video_soap_discuss.urls"
-
 WSGI_APPLICATION = "video_soap_discuss.wsgi.application"
-
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-
-STATIC_URL = "/static/"
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  "templates"),
-)
---
+++ video_soap_discuss/settings/dev.py
@@ -0,0 +1,24 @@
+from .common import *
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "../../db.sqlite3"),
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.7/howto/static-files/
+STATIC_URL = "/static/"
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, "../../templates"),
+)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+ALLOWED_HOSTS = []
