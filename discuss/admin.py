bf4dcce7acbc75c0213415a633b514d8a9d5ae25
Wayne Koorts <wayne@wkoorts.com>
2014-11-23 19:07:30 +1300

    Add initial admin pages.

--- discuss/admin.py
+++ discuss/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class SubjectAdmin(admin.ModelAdmin):
+    pass
+
+class DiscussionAdmin(admin.ModelAdmin):
+    pass
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class TranscriptionAdmin(admin.ModelAdmin):
+    pass
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+class RelatedLinkAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Discussion, DiscussionAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Transcription, TranscriptionAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(RelatedLink, RelatedLinkAdmin)
